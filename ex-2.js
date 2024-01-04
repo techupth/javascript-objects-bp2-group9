e7f67bf1051af12d6d830f39ceb230a467ee2ba6
KRITTIRIN <temzaaa4@gmail.com>
2024-01-04 20:16:38 +0700

    feat(Exercise) : Exerise's done by tem

--- ex-2.js
+++ ex-2.js
@@ -1,10 +1,20 @@
 // Exercise #2 Product
 
-const product = {
+let product = {
   name: "Xiaomi Air Purifier",
   price: 4000,
   ratings: 2.5,
   photo: null,
 };
+  product.ratings = 4.5;
+  product.photo = `https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg`;
+ product["code"] = "IT0101010"
+  delete product.ratings;
 let socialMediaShare = "facebookShare";
+product[socialMediaShare]  =  45.5;
+console.log(product)
+
 // Start code here
+
+
+
